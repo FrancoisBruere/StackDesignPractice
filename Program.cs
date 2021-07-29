c21302885d121ebd0d66c1ff7c410656f1f216f9
FrancoisBruere <85226595+FrancoisBruere@users.noreply.github.com>
2021-07-29 11:57:54 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackDesignPractice
+{
+
+    public class Stack
+    {
+
+        private readonly List<object> stack = new List<object>();
+        
+        public void Push(object obj)
+        {
+         
+
+            if (obj == null)
+            {
+                throw new InvalidOperationException("Object obj is null");
+            }
+            else
+            {
+                stack.Add(obj);
+            }
+            
+
+
+        }
+
+        public object Pop()
+        {
+            if (stack.Count == 0)
+            
+                throw new InvalidOperationException("stack list is empty");
+
+                var index = stack.Count - 1;
+                var stackIndex = stack[index];
+                
+                stack.RemoveAt(stack.Count - 1);
+                return stackIndex;
+           
+
+        }
+
+
+        public void Clear()
+        {
+
+            stack.Clear();
+
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var stack = new Stack();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+
+            stack.Clear();
+        }
+    }
+}
